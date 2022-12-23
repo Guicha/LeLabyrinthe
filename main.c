62c9e4850b7318e0bf1e4cffc450c234d9af6a89
Guicha <48026427+Guicha@users.noreply.github.com>
2022-12-23 01:29:40 +0100

    update
    ajout de la génération des trésors dans l'initialisation du plateau

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main() {
 
 
 
+
     // === ICONES DE TRESORS ET POSITIONS ===
 
         char fleche_haut[11] = "f_h";
@@ -23,20 +24,21 @@ int main() {
         char tuile_esp[11] = "         ";
 
 
+
     // === TUILES L ===
-        char tuile_L_1[11] = "# #"
+        char tuile_L_1[11] = "# #" // # ##  ###
                              "#  "
                              "###"; // ╚
 
-        char tuile_L_2[11] = "###"
+        char tuile_L_2[11] = "###" // ####  # #
                              "#  "
                              "# #"; // ╔
 
-        char tuile_L_3[11] = "###"
+        char tuile_L_3[11] = "###" // ###  ## #
                              "  #"
                              "# #"; // ╗
 
-        char tuile_L_4[11] = "# #"
+        char tuile_L_4[11] = "# #" // # #  ####
                              "  #"
                              "###"; // ╝
 
@@ -44,11 +46,11 @@ int main() {
 
 
     // === TUILES I ===
-        char tuile_I_1[11] = "# #"
+        char tuile_I_1[11] = "# #" // # ## ## #
                              "# #"
                              "# #"; // ║
 
-        char tuile_I_2[11] = "###"
+        char tuile_I_2[11] = "###" // ###   ###
                              "   "
                              "###"; // ═
 
@@ -56,25 +58,27 @@ int main() {
 
 
     // === TUILES T ===
-        char tuile_T_1[11] = "###"
+        char tuile_T_1[11] = "###" // ###   # #
                              "   "
                              "# #"; // ╦
 
-        char tuile_T_2[11] = "# #"
+        char tuile_T_2[11] = "# #" // # ##  # #
                              "#  "
                              "# #"; // ╠
 
-        char tuile_T_3[11] = "# #"
+        char tuile_T_3[11] = "# #" // # #  ## #
                              "  #"
                              "# #"; // ╣
 
-        char tuile_T_4[11] = "# #"
+        char tuile_T_4[11] = "# #" // # #   ###
                              "   "
                              "###"; // ╩
 
 
     // === TABLEAU/SAC DE TUILES ===
-    char *sac_de_tuiles[10] = {tuile_L_1, tuile_L_2, tuile_L_3, tuile_L_4, tuile_T_1, tuile_T_2, tuile_T_3, tuile_T_4, tuile_I_1, tuile_I_2};
+    char sac_de_tuiles[10][11] = {"# ##  ###", "####  # #", "###  ## #", "# #  ####", "###   # #", "# ##  # #", "# #  ## #", "# #   ###", "# ## ## #", "###   ###"};
+                                    //{tuile_L_1,         tuile_L_2,       tuile_L_3,     tuile_L_4,          tuile_T_1,      tuile_T_2,      tuile_T_3,       tuile_T_4,       tuile_I_1,       tuile_I_2}
+
 
     // === PLATEAU DE JEU ===
     char *plateau_de_jeu[81] = {tuile_esp, tuile_esp, fleche_bas, tuile_esp, fleche_bas, tuile_esp, fleche_bas, tuile_esp, tuile_esp,
@@ -90,14 +94,9 @@ int main() {
 
 
 
-
-
-
-
     // === VARIABLES MENU ===
     int choix = 0;
     int alive = 1;
-
 
 
     // === VARIABLES (NOUVELLE) PARTIE ===
@@ -122,14 +121,200 @@ int main() {
             if (init_partie == 1) {
 
                 // === CREATION DE LA PARTIE ===
-                creer_partie(&choix, &nombre_joueurs, tableau_joueurs);
+                //creer_partie(&choix, &nombre_joueurs, tableau_joueurs);
 
                 // === INITIALISATION DU PLATEAU ===
+                //init_plateau(plateau_de_jeu, sac_de_tuiles);
 
-                init_plateau(plateau_de_jeu, sac_de_tuiles);
+                // === INITIALISATION DU TABLEAU AVEC DE L'ALEATOIRE ===
+                /*
+                 * NOMBRE DE TUILES NECESSAIRES POUR L'ALEATOIRE DU DEBUT
+                 *
+                 * 6x tuiles en T (avec trésor)
+                 * 6x tuiles en L (avec trésor)
+                 * 10x tuiles en L
+                 * 12x tuiles en I
+                 *
+                 *
+                 *
+                 * */
 
-                init_partie = 0;
+                int i;
+                int selecteur_aleatoire;
+                int selecteur_interne;
+                int selecteur_incrementation = 0;
+                int index_tableau = 11;
 
+                int compteur_tuiles_T_tresor = 0;
+                int compteur_tuiles_L_tresor = 0;
+                int compteur_tuiles_L = 0;
+                int compteur_tuiles_I = 0;
+
+                char *chaine_temp;
+
+                // === INDICATEURS DE TUILES ===
+                // = TUILES L =
+                int araignee = 99;
+                int insecte = 99;
+                int hibou = 99;
+                int souris = 99;
+                int lezard = 99;
+                int scarabee = 99;
+
+                for (i=0; i<34; i++) {
+
+                    chaine_temp = malloc(11 * sizeof (char));
+
+                    do {
+
+                        selecteur_aleatoire = rand() % (9 - 0 + 1) + 0;
+
+                    } while ( ((selecteur_aleatoire >= 0 && selecteur_aleatoire <= 3) && compteur_tuiles_L == 16) || ((selecteur_aleatoire >= 4 && selecteur_aleatoire <= 7) && compteur_tuiles_T_tresor == 6) || ((selecteur_aleatoire >= 8 && selecteur_aleatoire <= 9) && compteur_tuiles_I == 12));
+
+
+
+                    if (selecteur_aleatoire >= 0 && selecteur_aleatoire <= 3) { // On tombe sur les tuiles L (sans et avec tresor)
+
+                        selecteur_interne = rand() % (1 - 0 + 1) + 0;
+
+                        if (selecteur_interne == 1) {
+
+                            if (compteur_tuiles_L_tresor < 6) {
+
+                                strcpy(chaine_temp, sac_de_tuiles[selecteur_aleatoire]);
+
+                                do {
+
+                                    selecteur_interne = rand() % (5 - 0 + 1) + 0;
+
+                                } while (selecteur_interne == araignee || selecteur_interne == insecte || selecteur_interne == hibou ||selecteur_interne == souris || selecteur_interne == souris || selecteur_interne == lezard || selecteur_interne == scarabee);
+
+
+                                if (selecteur_interne == 0) {
+
+                                    chaine_temp[4] = 2; // Araignée
+                                    araignee = 0;
+
+                                } else if (selecteur_interne == 1) {
+
+                                    chaine_temp[4] = 'I'; // Insecte
+                                    insecte = 1;
+
+                                } else if (selecteur_interne == 2) {
+
+                                    chaine_temp[4] = 'H'; // Hibou
+                                    hibou = 2;
+
+                                } else if (selecteur_interne == 3) {
+
+                                    chaine_temp[4] = 'S'; // Souris
+                                    souris = 3;
+
+                                } else if (selecteur_interne == 4) {
+
+                                    chaine_temp[4] = 'L'; // Lézard
+                                    lezard = 4;
+
+                                } else if (selecteur_interne == 5) {
+
+                                    chaine_temp[4] = 's'; // Scarabée
+                                    scarabee = 5;
+
+                                }
+
+
+                                plateau_de_jeu[index_tableau] = chaine_temp;
+
+                                compteur_tuiles_L_tresor = compteur_tuiles_L_tresor + 1;
+
+
+                            } else {
+
+                                plateau_de_jeu[index_tableau] = sac_de_tuiles[selecteur_aleatoire];
+                            }
+
+                        } else if (selecteur_interne == 0) {
+
+                            plateau_de_jeu[index_tableau] = sac_de_tuiles[selecteur_aleatoire];
+                        }
+
+                        compteur_tuiles_L = compteur_tuiles_L + 1;
+
+
+
+
+                    } else if (selecteur_aleatoire >= 4 && selecteur_aleatoire <= 7) { // On tombe sur les tuiles T (avec tresor)
+
+                        plateau_de_jeu[index_tableau] = sac_de_tuiles[selecteur_aleatoire];
+
+                        compteur_tuiles_T_tresor = compteur_tuiles_T_tresor + 1;
+
+                    } else if (selecteur_aleatoire >= 8 && selecteur_aleatoire <= 9) { // On tombe sur les tuiles I
+
+                        plateau_de_jeu[index_tableau] = sac_de_tuiles[selecteur_aleatoire];
+
+                        compteur_tuiles_I = compteur_tuiles_I + 1;
+
+                    }
+
+
+
+                    if (index_tableau == 15) {
+
+                        index_tableau = 18;
+
+                        selecteur_incrementation = 1;
+
+                    } else if (index_tableau == 25) {
+
+                        index_tableau = 27;
+
+                        selecteur_incrementation = 0;
+
+                    } else if (index_tableau == 33) {
+
+                        index_tableau = 36;
+
+                        selecteur_incrementation = 1;
+
+                    } else if (index_tableau == 43) {
+
+                        index_tableau = 45;
+
+                        selecteur_incrementation = 0;
+
+                    } else if (index_tableau == 51) {
+
+                        index_tableau = 54;
+
+                        selecteur_incrementation = 1;
+
+                    } else if (index_tableau == 61) {
+
+                        index_tableau = 63;
+
+                        selecteur_incrementation = 0;
+                    }
+
+
+
+                    if (selecteur_incrementation == 0) {
+
+                        index_tableau = index_tableau + 2;
+
+                    } else if (selecteur_incrementation == 1) {
+
+                        index_tableau = index_tableau + 1;
+                    }
+
+
+
+
+                }
+
+                free(chaine_temp);
+
+                //init_partie = 0;
 
             }
 
