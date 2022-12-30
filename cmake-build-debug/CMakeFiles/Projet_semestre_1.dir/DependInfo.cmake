16fed90a5b4f26320a7a95b55b19dcb2e0738a9b
Yomanacoder <120833021+Yomanacoder@users.noreply.github.com>
2022-12-30 09:57:13 +0100

    Update main.c

---
+++ cmake-build-debug/CMakeFiles/Projet_semestre_1.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/yonat/OneDrive/Documents/GitHub/Projet-Algo-Semestre-1/Affichagecomptagetresor.c" "CMakeFiles/Projet_semestre_1.dir/Affichagecomptagetresor.c.obj" "gcc" "CMakeFiles/Projet_semestre_1.dir/Affichagecomptagetresor.c.obj.d"
+  "C:/Users/yonat/OneDrive/Documents/GitHub/Projet-Algo-Semestre-1/main.c" "CMakeFiles/Projet_semestre_1.dir/main.c.obj" "gcc" "CMakeFiles/Projet_semestre_1.dir/main.c.obj.d"
+  "C:/Users/yonat/OneDrive/Documents/GitHub/Projet-Algo-Semestre-1/src/Menu/color.c" "CMakeFiles/Projet_semestre_1.dir/src/Menu/color.c.obj" "gcc" "CMakeFiles/Projet_semestre_1.dir/src/Menu/color.c.obj.d"
+  "C:/Users/yonat/OneDrive/Documents/GitHub/Projet-Algo-Semestre-1/src/Menu/consulter_sauvegardes.c" "CMakeFiles/Projet_semestre_1.dir/src/Menu/consulter_sauvegardes.c.obj" "gcc" "CMakeFiles/Projet_semestre_1.dir/src/Menu/consulter_sauvegardes.c.obj.d"
+  "C:/Users/yonat/OneDrive/Documents/GitHub/Projet-Algo-Semestre-1/src/Menu/credits-regles.c" "CMakeFiles/Projet_semestre_1.dir/src/Menu/credits-regles.c.obj" "gcc" "CMakeFiles/Projet_semestre_1.dir/src/Menu/credits-regles.c.obj.d"
+  "C:/Users/yonat/OneDrive/Documents/GitHub/Projet-Algo-Semestre-1/src/Menu/cursor.c" "CMakeFiles/Projet_semestre_1.dir/src/Menu/cursor.c.obj" "gcc" "CMakeFiles/Projet_semestre_1.dir/src/Menu/cursor.c.obj.d"
+  "C:/Users/yonat/OneDrive/Documents/GitHub/Projet-Algo-Semestre-1/src/Menu/menu_ingame.c" "CMakeFiles/Projet_semestre_1.dir/src/Menu/menu_ingame.c.obj" "gcc" "CMakeFiles/Projet_semestre_1.dir/src/Menu/menu_ingame.c.obj.d"
+  "C:/Users/yonat/OneDrive/Documents/GitHub/Projet-Algo-Semestre-1/src/Menu/menu_principal.c" "CMakeFiles/Projet_semestre_1.dir/src/Menu/menu_principal.c.obj" "gcc" "CMakeFiles/Projet_semestre_1.dir/src/Menu/menu_principal.c.obj.d"
+  "C:/Users/yonat/OneDrive/Documents/GitHub/Projet-Algo-Semestre-1/src/NouvellePartie/afficher_plateau.c" "CMakeFiles/Projet_semestre_1.dir/src/NouvellePartie/afficher_plateau.c.obj" "gcc" "CMakeFiles/Projet_semestre_1.dir/src/NouvellePartie/afficher_plateau.c.obj.d"
+  "C:/Users/yonat/OneDrive/Documents/GitHub/Projet-Algo-Semestre-1/src/NouvellePartie/creer_partie.c" "CMakeFiles/Projet_semestre_1.dir/src/NouvellePartie/creer_partie.c.obj" "gcc" "CMakeFiles/Projet_semestre_1.dir/src/NouvellePartie/creer_partie.c.obj.d"
+  "C:/Users/yonat/OneDrive/Documents/GitHub/Projet-Algo-Semestre-1/src/NouvellePartie/init_plateau.c" "CMakeFiles/Projet_semestre_1.dir/src/NouvellePartie/init_plateau.c.obj" "gcc" "CMakeFiles/Projet_semestre_1.dir/src/NouvellePartie/init_plateau.c.obj.d"
+  "C:/Users/yonat/OneDrive/Documents/GitHub/Projet-Algo-Semestre-1/src/NouvellePartie/notice_joueurs.c" "CMakeFiles/Projet_semestre_1.dir/src/NouvellePartie/notice_joueurs.c.obj" "gcc" "CMakeFiles/Projet_semestre_1.dir/src/NouvellePartie/notice_joueurs.c.obj.d"
+  "C:/Users/yonat/OneDrive/Documents/GitHub/Projet-Algo-Semestre-1/src/NouvellePartie/options_joueur.c" "CMakeFiles/Projet_semestre_1.dir/src/NouvellePartie/options_joueur.c.obj" "gcc" "CMakeFiles/Projet_semestre_1.dir/src/NouvellePartie/options_joueur.c.obj.d"
+  "C:/Users/yonat/OneDrive/Documents/GitHub/Projet-Algo-Semestre-1/src/Testing/afficher_tab_struct_joueurs.c" "CMakeFiles/Projet_semestre_1.dir/src/Testing/afficher_tab_struct_joueurs.c.obj" "gcc" "CMakeFiles/Projet_semestre_1.dir/src/Testing/afficher_tab_struct_joueurs.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
